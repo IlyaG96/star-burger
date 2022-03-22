bdd40f63c32281e31b8cc2cdab0cc530463bc85c
IlyaG96 <ilyag_96@icloud.com>
2022-03-22 22:39:12 +1100

    rewrite serializers

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -13,14 +13,7 @@ class OrderElementsSerializer(ModelSerializer):
         return OrderElements.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-
-        instance.product = validated_data.get('product', instance.product)
-        instance.quantity = validated_data.get('quantity', instance.quantity)
-        instance.save()
-
-        return instance
-
-
+        return OrderElements.objects.update(**validated_data)
 
 
 class OrderSerializer(ModelSerializer):
@@ -35,14 +28,7 @@ class OrderSerializer(ModelSerializer):
         return Order.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-
-        instance.firstname = validated_data.get('name', instance.name)
-        instance.lastname = validated_data.get('lastname', instance.lastname)
-        instance.phonenumber = validated_data.get('phonenumber', instance.phonenumber)
-        instance.address = validated_data.get('address', instance.address)
-        instance.save()
-
-        return instance
+        return Order.objects.update(**validated_data)
 
     def validate_products(self, products):
         if not products:
