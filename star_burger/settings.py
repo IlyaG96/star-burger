8beaf20e1df4d8a7f25b046ecc2f8b6002ca60f8
IlyaG96 <ilyag_96@icloud.com>
2022-04-09 20:12:48 +1100

    turn on rollbar

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-  #  'rollbar.contrib.django.middleware.RollbarNotifierMiddlewareExcluding404'
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddlewareExcluding404'
 ]
 
 ROOT_URLCONF = 'star_burger.urls'
